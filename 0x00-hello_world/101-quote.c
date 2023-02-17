39d823527b585aae67a2fc42fc88f56c4fc32b73
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-16 16:41:56 -0800

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Writes to stderr
+ * Return: 1
+*/
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
+	return (1);
+}
