52de520ba94efc4febc276d640de940d580d6076
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-17 11:49:55 -0800

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Checks if a number is bigger or less than 0
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is negative");
+	} else if (n < 0)
+	{
+		printf("%d is positive");
+	} else
+	{
+		printf("%d is zero");
+	}
+	return (0);
+}
