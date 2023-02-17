147156a8513cf925d800eb81b79180ef21ed7484
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-17 11:56:40 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is negative");
+		printf("%d is positive", n);
 	} else if (n < 0)
 	{
-		printf("%d is positive");
+		printf("%d is negative", n);
 	} else
 	{
-		printf("%d is zero");
+		printf("%d is zero", n);
 	}
 	return (0);
 }
