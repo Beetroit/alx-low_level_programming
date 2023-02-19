38af0ba8c907cfba967fe6aff634effdbaa50816
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-19 10:01:00 -0800

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints unique two digit combos
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (j = 49; j < 57; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (i < j && j < k && j != k && i != j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 56 || j < 57 || k < 58)
+					{
+						putchar(44);
+						putchar(32);
+					}
+
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
