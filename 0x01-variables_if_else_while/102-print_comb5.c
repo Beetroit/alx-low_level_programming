caa440d35a5bc05d5e3544a296b3b5dffac45741
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-19 19:37:37 -0800

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Having fun with numbers
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			if (((i / 10) + (i % 10)) < ((j / 10) + (j % 10)))
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(32);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (((i / 10) + (i % 10) + (j / 10) + (j % 10)) != 35)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
