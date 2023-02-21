72df6c7a088542a349d30205eaf4e39115af6078
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-21 08:47:46 -0800

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "./main.h"
+/**
+ * main - Prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+	int size = sizeof(str);
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+	return (0);
+}
