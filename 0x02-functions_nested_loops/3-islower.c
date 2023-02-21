e8ad52a3db9e015bcddfe73806bda2df3f5afb74
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-21 21:29:01 +0100

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,22 @@
-#include "./main.h"
+#include "main.h"
 /**
- * is_lower - Checks if input is lowercase
- * @c: Char to be checked
- * Return: 1 if True, 0 otherwise
- */
-
+ * is_lower - checks if input is lowercase
+ * @c: input to be checked
+ * 
+ * Return: 1 if lower, 0 otherwise
+*/
 int is_lower(int c)
 {
 	int i;
+	int result = 0;
 
 	for (i = 97; i <= 122; i++)
 	{
 		if (c == i)
 		{
-			return (1);
-		} else if (i == 122 || i != c)
-		{
-			return (0);
+			result = 1;
 		}
 	}
-	_putchar(10);
+	return (result);
 }
+
