f5f162b3ef8380156be0c681183abf38cb8be3cb
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-21 09:59:53 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "./main.h"
+/**
+ * main - Prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int size = sizeof(str);
+
+	for (i = 0; i < size - 1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
