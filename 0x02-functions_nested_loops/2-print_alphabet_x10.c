f7ba56ad8c45980cc6b471933f538cfe5f2c1284
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-21 11:07:52 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 
 	for (j = 0; j < 11; j++)
 	{
-		for (i = 97; i < 122; i++)
+		for (i = 97; i < 123; i++)
 		{
 			_putchar(i);
 		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
