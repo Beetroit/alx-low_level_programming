14ecfd24fd295cc8bcb3854265959f93dc2486f9
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-21 22:58:33 +0100

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign attached to a number
+ * @n: The number to be checked
+ * 
+ * Return: 1 or -1 if pos or neg, else 0 if 0.
+ */
+int print_sign(int n)
+{
+	int res = 0;
+
+	if (n > 0)
+	{
+		putchar(43);
+		res = 1;
+	} else if (n < 0)
+	{
+		putchar(45);
+		res = -1;
+	} else if (n == 0)
+	{
+		putchar(48);
+	}
+	return (res);
+}
