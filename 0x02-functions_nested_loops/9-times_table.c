a311112f78a9b581915533ed0147df9dd057773b
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-22 05:44:14 +0100

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,13 +4,13 @@
  *
  * Return: 0
 */
-int times_table(void)
+int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			printf("%2d", i * j);
 			if (j != 9)
