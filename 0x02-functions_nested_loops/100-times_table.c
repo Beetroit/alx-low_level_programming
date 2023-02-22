b8bdae9a064b36bf08f9d4830b274ec0ab148a2e
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-22 19:25:28 +0100

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,9 @@
 #include "main.h"
 /**
- * times_table - Prints n times table
+ * print_times_table - Prints n times table
  *
  * Return: Always 0.
  */
-void table(int n)
-{
-	int a, b, c;
-
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			c = a * b;
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
-			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
-		}
-		_putchar ('\n');
-	}
-}
 void print_times_table(int n)
 {
 	if (n <= 0 || n >= 15)
@@ -51,7 +11,43 @@ void print_times_table(int n)
 		return;
 	} else
 	{
-		table(n);
+		int a, b, c;
+
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if ((c / 10) == 0)
+				{
+					if (b == 0)
+					{
+						_putchar ('0');
+					}
+					if (b != 0)
+					{
+						_putchar (' ');
+						_putchar ((c % 10) + '0');
+					}
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
+				else
+				{
+					_putchar ((c / 10) + '0');
+					_putchar ((c % 10) + '0');
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
+			}
+			_putchar ('\n');
+		}
 		_putchar(10);
 	}
 }
