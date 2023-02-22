c68846be64276dc06c1c04a0cfc06e1fc9cb57fc
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-23 00:16:30 +0100

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-	unsigned long long int a = 1;
-	unsigned long long int b = 2;
+	long int a = 1;
+	long int b = 2;
 	int n = 50;
-	unsigned long long int tmp;
+	long int tmp;
 
 	while (n > 0)
 	{
-		printf("%llu", a);
+		printf("%lu", a);
 		tmp = a;
 		if (n > 1)
 		{
