a6693dbc26ea70ed2b4f1e2099c4c449db17d287
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-22 20:29:24 +0100

    task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints sum of multiples of 3 or 5 
+ * Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+	int _sum = 0;
+
+	while (i <= 1023)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			_sum += i;
+			printf("%d\n", _sum);
+		}
+		i++;
+		
+	}
+	printf("%d\n", _sum);
+	return (0);
+}
