44fe1fba59727c1d168fa70de33e6c7fae1cdcf0
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-23 18:02:09 +0100

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if input is uppercase or not
+ * @c: Input to be checked
+ *
+ * Return: 0
+*/
+
+int _isdigit(int c)
+{
+	int val = 0;
+
+	if (c >= 48 && c <= 58)
+	{
+		val = 1;
+	}
+	return (val);
+}
