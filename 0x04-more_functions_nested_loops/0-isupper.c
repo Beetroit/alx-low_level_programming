398ed4b8d43f022f4151e61471b2e2f0c10f30ae
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-23 17:53:49 +0100

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if input is uppercase or not
+ * @c: Input to be checked
+ *
+ * Return: 0
+*/
+
+int _isupper(int c)
+{
+	int val = 0;
+	if (c >= 65 && c <= 90)
+	{
+		val =1;
+	}
+	return (val);
+}
