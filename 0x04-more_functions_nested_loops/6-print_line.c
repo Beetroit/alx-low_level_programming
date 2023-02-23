eaaf85e057961bc12f959a8df1aafc3f44db78ec
Daniel Obiorah <Beetroit3266@gmail.com>
2023-02-23 21:48:13 +0100

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,15 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar(10);
+		_putchar('\n');
 	} else
 	{
-		while (n > 0)
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
-			n--;
 		}
-		_putchar(10);
-	}	
+		_putchar('\n');
+	}
 }
