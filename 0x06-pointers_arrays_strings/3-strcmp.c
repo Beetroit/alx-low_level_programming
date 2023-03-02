e580852431fbae10f1bdab2e5600143153f7f458
Daniel Obiorah <Beetroit3266@gmail.com>
2023-03-02 19:22:32 +0100

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: A value equal to the difference of the strings
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
