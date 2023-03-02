183be8233409324ba9b489a30e5594cfdbee8a62
Daniel Obiorah <Beetroit3266@gmail.com>
2023-03-02 13:18:15 +0100

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - Returns a concatenated string
+ * @src: Source string
+ * @dest: Destination string
+ * @n: Number of bytes
+ * Return: Concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	if (j < n)
+	{
+		dest[i] = '\0';
+	}
+	return dest;
+}
