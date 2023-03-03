5b4f440f763ed95a6d130a4e54be97def779d13f
Daniel Obiorah <Beetroit3266@gmail.com>
2023-03-03 03:11:28 +0100

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,14 @@
 #include "main.h"
+/**
+ * cap_strings - Converts words to title case
+ * @str: string to be converted
+ *
+ * Return: Title-cased string
+*/
 char *cap_string(char *str)
 {
 	int i;
+	int j;
 	char separators[] = " \t\n,;.!?\"(){}";
 
 	if (str[0] >= 'a' && str[0] <= 'z')
@@ -9,7 +16,7 @@ char *cap_string(char *str)
 
 	for (i = 1; str[i]; i++)
 	{
-		for (int j = 0; separators[j]; j++)
+		for (j = 0; separators[j]; j++)
 		{
 			if (str[i - 1] == separators[j] && str[i] >= 'a' && str[i] <= 'z')
 			{
